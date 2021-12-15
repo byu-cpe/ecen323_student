1e6e5009b03c6da102baa5e31bc5ffd1007e4862
wirthlin <wirthlin@byu.edu>
2021-12-14 17:12:53 -0700

    Fixed white space

--- lab01/buttoncount/ButtonCount.sv
+++ lab01/buttoncount/ButtonCount.sv
@@ -6,11 +6,13 @@
 
 /***************************************************************************
 * 
-* Module: ButtonCount.sv
+* File: ButtonCount.sv
 *
 * Author: Professor Mike Wirthlin
-* Class: ECEN 323, Winter Semester 2020
+* Class: ECEN 323
 * Date: 12/10/2020
+*
+* Module: ButtonCount
 *
 * Description:
 *    This module includes a state machine that will provide a one cycle
@@ -29,52 +31,52 @@ module ButtonCount(clk, btnc, btnu, led);
 	input wire logic clk, btnc, btnu;
 	output logic [15:0] led;
 	
-    // Constants for the state machine state assignments.
-    //  CODING STANDARD: You should always use a named constant (localparam)
-    //  rather than "magic constants" in the body of your RTL code.
+	// Constants for the state machine state assignments.
+	//  CODING STANDARD: You should always use a named constant (localparam)
+	//  rather than "magic constants" in the body of your RTL code.
 	localparam ZERO = 2'b00;
 	localparam INC = 2'b01;
 	localparam ONE = 2'b10;
 
-    // The internal 16-bit count signal. 
+	// The internal 16-bit count signal. 
 	logic [15:0] count_i;
-    // The increment counter output from the state machine
+	// The increment counter output from the state machine
 	logic inc_count;
-    // The current state and next state of the state machine
+	// The current state and next state of the state machine
 	logic [1:0] state, next_state;
+	// Internal reset and increment signals
+	logic rst, inc;
 
-    logic rst, inc;
-
-    assign rst = btnc;
-    assign inc = btnu;
+	assign rst = btnc;
+	assign inc = btnu;
 
 	// 16-bit Counter. Increments once each time button is pressed. 
-    //
-    // This is an exmaple of a 'sequential' statement that will synthesize flip-flops
-    // as well as the logic for incrementing the count value.
-    //
-    //  CODING STANDART: Every "segment/block" of your RTL code must have at least
-    //  one line of white space between it and the previous and following block. Also,
-    //  ALL always blocks must have a coment.
+	//
+	// This is an exmaple of a 'sequential' statement that will synthesize flip-flops
+	// as well as the logic for incrementing the count value.
+	//
+	//  CODING STANDART: Every "segment/block" of your RTL code must have at least
+	//  one line of white space between it and the previous and following block. Also,
+	//  ALL always blocks must have a coment.
 	always_ff@(posedge clk)
 		if (rst)
 			count_i <= 0;
 		else if (inc_count)
 			count_i <= count_i + 1;
-    
-    // Assign the 'led' output the value of the internal count_i signal.
+	
+	// Assign the 'led' output the value of the internal count_i signal.
 	assign led = count_i;
 
-    // Button state machine
-    //  This state machine is used to detect the first zero to one transition
-    //  on the button input signal. When this transition occurs, the output signal
-    //  will be asserted and used to increment the counter. This state machine is
-    //  necessary to make sure that the counter is incremented only once for each
-    //  button press. This is an exmaple of a "Moore" state machine (outputs only
-    //  depend on current state and not the inputs).
+	// Button state machine
+	//  This state machine is used to detect the first zero to one transition
+	//  on the button input signal. When this transition occurs, the output signal
+	//  will be asserted and used to increment the counter. This state machine is
+	//  necessary to make sure that the counter is incremented only once for each
+	//  button press. This is an exmaple of a "Moore" state machine (outputs only
+	//  depend on current state and not the inputs).
 
 	// State register for button state machine. This sequential code will synthesize
-    // the flip flops for the state register.
+	// the flip flops for the state register.
 	always_ff@(posedge clk)
 		if (rst)
 			state = ZERO;
@@ -82,42 +84,42 @@ module ButtonCount(clk, btnc, btnu, led);
 			state = next_state;
 
 	// Next state logic for state machine
-    //  This is a *combinational* circuit - no flip-flops or state are synthesized
-    //  for this statement. 
+	//  This is a *combinational* circuit - no flip-flops or state are synthesized
+	//  for this statement. 
 	always_comb begin
-        // Default assignment statement (stay in the same state)
+		// Default assignment statement (stay in the same state)
 		next_state = state;
-        // Case statement for each state to override the default next_state
-        // assignment.
+		// Case statement for each state to override the default next_state
+		// assignment.
 		case(state)
-            // The ZERO state occurs when the button is not pressed (zero) and 
-            // will stay in this state until the button is first pressed.
+			// The ZERO state occurs when the button is not pressed (zero) and 
+			// will stay in this state until the button is first pressed.
 			ZERO:
-                // Transition to the INC state when the 'inc' signal is high
-                // (otherwise stay in this state due to the default assignment statement)
+				// Transition to the INC state when the 'inc' signal is high
+				// (otherwise stay in this state due to the default assignment statement)
 				if (inc)
 					next_state = INC;
-            // The INC state occurs when the button is first pressed. The state machine
-            // will only be in this state for one clock cycle and move directly to either the 
-            // ONE state or the ZERO state.
+			// The INC state occurs when the button is first pressed. The state machine
+			// will only be in this state for one clock cycle and move directly to either the 
+			// ONE state or the ZERO state.
 			INC:
-                if (inc)
-				    next_state = ONE;
-                else
-                    next_state = ZERO;
-            // The ONE state occurs when the button is being pressed. The state machine
-            // will stay in this state until the button is released.
+				if (inc)
+					next_state = ONE;
+				else
+					next_state = ZERO;
+			// The ONE state occurs when the button is being pressed. The state machine
+			// will stay in this state until the button is released.
 			ONE:
-                // Transition to the ZERO state when the 'inc' signal is low. Otherwise
-                //   stay in this state.
+				// Transition to the ZERO state when the 'inc' signal is low. Otherwise
+				//   stay in this state.
 				if (!inc)
 					next_state = ZERO;
 		endcase
 	end
 
-    // Output forming logic. This combinational logic will set the value of the 
-    // "inc_count" signal high when the current state of the state machine is in the
-    // "INC" state. 
+	// Output forming logic. This combinational logic will set the value of the 
+	// "inc_count" signal high when the current state of the state machine is in the
+	// "INC" state. 
 	assign inc_count = (state == INC);
 	
 endmodule
