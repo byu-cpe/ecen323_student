106936e9298d77064f15817c8b8b843a2f3f3f48
Mike Wirthlin <wirthlin@byu.edu>
2022-02-16 12:51:44 -0700

    Working script

--- lab09/lab09_passoff.py
+++ lab09/lab09_passoff.py
@@ -7,7 +7,6 @@ import sys
 # Add lab passoff files
 resources_path = pathlib.Path(__file__).resolve().parent.parent  / 'resources'
 sys.path.append( str(resources_path) )
-#sys.path.append('../resources')
 import lab_passoff
 import tester_module
 
@@ -25,35 +24,35 @@ SCRIPT_PATH = pathlib.Path(__file__).absolute().parent.resolve()
 # used to represent a specific file for the lab. The value is the path and filename
 # (relative to the lab directory) of the file to include in the submission.
 submission_files = {
-    "pipeline"		: "riscv_basic_pipeline.sv",
+    "forwarding"		: "riscv_forwarding_pipeline.sv",
 }
 
 # List of files needed for testing that should be in the repository.
 # The key is a lab-specific keyword used to represent a specific file for the lab. 
 # The value is the name of the file (relative to the lab directory)
 test_files = {
-	"riscv_pipeline_tb"		: "riscv_pipeline_tb.sv",
-	"pipeline_nop"			: "pipeline_nop.s",
+	"riscv_forwarding_tb"	: "riscv_forwarding_tb.sv",
+	"forwarding_asm"		: "forwarding.s",
     "alu"           		: "../lab02/alu.sv",
     "alu_constants"     	: "../lab02/riscv_alu_constants.sv",
     "regfile"       		: "../lab03/regfile.sv",
 }
 
 # Assembly
-pipeline_nop_mem = tester_module.rars_mem_file("pipeline_nop", generate_data_mem=True)
+forwarding_mem = tester_module.rars_mem_file("forwarding_asm", generate_data_mem=True)
 
 # TCL simulations
 
 # Testbench simulations
-pipeline_tb = tester_module.testbench_simulation( "Pipeline Testbench", \
-	"riscv_pipeline_tb", \
-	[ "riscv_pipeline_tb", "alu_constants", "alu",  "regfile", "pipeline",   ], [], \
-		 include_dirs = ["../lab02",], )
+forwarding_tb = tester_module.testbench_simulation( "Forwarding Testbench", \
+	"riscv_forwarding_tb", \
+	[ "riscv_forwarding_tb", "alu_constants", "alu",  "regfile", "forwarding",   ], [], \
+		 include_dirs = ["../lab02", "../include"], )
 
 # Synthesis batches
-pipeline_build = tester_module.build_bitstream( "riscv_basic_pipeline", [], 
-	[ "alu_constants", "alu",  "regfile", "pipeline", ], False, False, \
-		include_dirs=["../lab02",])
+forwarding_build = tester_module.build_bitstream( "riscv_forwarding_pipeline", [], 
+	[ "alu_constants", "alu",  "regfile", "forwarding", ], False, False, \
+		include_dirs=["../lab02","../include"])
 
 # Bitstream build
 
@@ -68,9 +67,9 @@ def main():
 	# Prepare test
 	lab_test.prepare_test(submission_files,test_files)
 	# Add tests
-	lab_test.add_test_module(pipeline_nop_mem)
-	lab_test.add_test_module(pipeline_tb)
-	lab_test.add_test_module(pipeline_build)
+	lab_test.add_test_module(forwarding_mem)
+	lab_test.add_test_module(forwarding_tb)
+	lab_test.add_test_module(forwarding_build)
 	# Run tests
 	lab_test.run_tests()
 
