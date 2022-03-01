650dfb50dcd61f3902dd7565ac3d811bd7e202c0
Mike Wirthlin <wirthlin@byu.edu>
2022-03-01 09:24:59 -0700

    Renamed build script

--- lab07/create_multicycle_project_v.tcl
+++ lab07/create_multicycle_project_v.tcl
@@ -1,14 +1,16 @@
 # Create project, specify part, and update project settings
-create_project -force forwarding_iov ./projv
+create_project -force multicycle_io_v ./projv
 set_property "part" "xc7a35tcpg236-1" [get_projects [current_project]]
 source ../resources/new_project_settings.tcl
 # Add the top-level I/O system and constraints file (provided in the lab)
-add_files forwarding_iosystem.sv
+add_files multicycle_iosystem.sv
 add_files -fileset constrs_1 ../resources/iosystem/iosystem.xdc
 # Add files from your previous labs and set the include directories
-add_files ../lab09/riscv_forwarding_pipeline.sv
+add_files ../lab06/riscv_multicycle.sv
+add_files ../lab05/riscv_simple_datapath.sv ../lab05/riscv_datapath_constants.sv
 add_files ../lab03/regfile.sv ../lab02/alu.sv ../lab02/riscv_alu_constants.sv
-set_property include_dirs {../lab02 ../include} [current_fileset]
+set_property include_dirs {../lab02 ../lab05} [current_fileset]
+# Add the files associated with the top-level I/O system
 # Add the files associated with the top-level I/O system
 add_files ../resources/iosystem/iosystem.sv
 add_files ../resources/iosystem/io_clocks.sv
