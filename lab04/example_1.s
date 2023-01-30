913bb01066c891124cfe262e1682665599e4e4c9
spriteman1200 <spriteman1200@gmail.com>
2023-01-30 11:01:30 -0700

    Placed detailed comments with proper spacing

--- lab04/example_1.s
+++ lab04/example_1.s
@@ -1,41 +1,51 @@
-################################
-# example_1.s
-# 
-# Simple Factorial Program
+#########################################################################
 #
-################################
+# Filename: example_1.s
+#
+# Author: Mike Wirthlin
+# Date: 1/22/2020
+# 
+# Description: Program to calculate the factorial for given non-zero, 
+# non-negative integer input (output = input!) without system calls
+#
+#########################################################################
 
 .globl main
+
+# Global data segment
 .data
 
-input:                      # The location for the input data
-    .word 4                 # allocates 4 byte set to the value 4
+input:                      # The location for the input factorial value
+    .word 4                 # Allocates 4 bytes and sets the input to 4 (arbitrary)
     
-output:                     # The location for the output data
-    .word 0                 # allocates 4 byte set to the value 0
+output:                     # The location for the output calculated factorial 
+                            # value given the input value data
+    .word 0                 # Allocates 4 bytes and sets the output to 0 for initialization
 
 .text
 main:                       # Label for start of program
     
-    lw a0,input             # Loads the input value from memory
+    lw a0,input             # Loads the desired input value from memory to compute the factorial
     
     li t0,1                 # Loads the value 1 into a t0 (a temporary constant)
-    ble a0,t0,done_fact     # If input is 1 or less, then skip to end	
+    ble a0,t0,done_fact     # Check if input is 1 or less, if so then the factorial is already computed since
+	                        # input = 1 results in output = 1, jump to the end of program otherwise compute the factorial	
     addi t0,a0,-1           # Load the value input-1 into t0 (loop counter variable)
-    li a0,1                 # Load 1 as initial output value (a0)
-    li t2,1                 # Load 1 (t2 is number to multiply by)
+    li a0,1                 # Load 1 as initial output value a0 (result) since all factorials contain 1 in the factorial product
+    li t2,1                 # Load 1 (t2 is number to multiply by, the next factorial operand (fo) to multiple the current result)
 
-do_fact:
-    blez t0,done_fact       # If t0 is less than or equal to zero then jump to end
-    addi t2,t2,1            # Increment t2 by 1
-    mul a0,a0,t2            # Multiply t2 by a0 to get next factorial
-    addi t0,t0,-1           # decrement t0 
-    j do_fact               # jump to top of loop to do next factorial until t0 is 0
+do_fact:                    # Performs the factorial when n > 0,1. Multiplies (result * fo) into a single product where
+                            # fo increments per loop execution. Loop terminates when (loop counter) <= 0
+    blez t0,done_fact       # If t0 (loop counter) is less than or equal to zero then jump to end otherwise continue
+    addi t2,t2,1            # Increment t2 (fo) by 1 to obtain next factorial operand
+    mul a0,a0,t2            # Multiply t2 (fo) by a0 (result) to get next factorial
+    addi t0,t0,-1           # decrement t0 (loop counter)
+    j do_fact               # jump to top of loop to do next factorial until t0 (loop counter) is 0
     
-done_fact:
+done_fact:                  # The factorial is finished, load output address and store result at output address
     la t0,output            # Load output address to t0
-    sw a0,0(t0)             # Save output value to output memory location
+    sw a0,0(t0)             # Save the calculated factorial result to output memory location
     
-exit_loop:
+exit_loop:                  # The factorial has finished computing, remain in endless loop
     j exit_loop             # Loop Exit
 
