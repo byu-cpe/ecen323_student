9c8ce260a8d3b452a2fad995711941f1d596d911
Mike Wirthlin <wirthlin@byu.edu>
2024-01-03 12:31:35 -0700

    Downgrade synthesis message

--- resources/new_project_settings.tcl
+++ resources/new_project_settings.tcl
@@ -31,6 +31,9 @@ set_msg_config -new_severity "INFO" -id "DRC RTSTAT-10"
 # Downgrade the waraning 'WARNING: [Synth 8-3331] design riscv_simple_datapath has unconnected port instruction[14]' to INFO
 #  These start in lab 5
 set_msg_config -new_severity "INFO" -id "Synth 8-3331"
+# Downgrade: WARNING: [Synth 8-7080] Parallel synthesis criteria is not met
+set_msg_config -new_severity "INFO" -id "Synth 8-7080"
+
 # Other possible downgrade options: (lab 9 warnings in synthesis)
 # WARNING: [Synth 8-6014] Unused sequential element mem_ALUSrc_reg was removed.  [/tmp/ecen323_wirthlin/lab09/riscv_forwarding_pipeline.sv:320]
 # WARNING: [Synth 8-7023] instance 'my_alu' of module 'alu' has 5 connections declared, but only 4 given [/tmp/ecen323_wirthlin/lab09/riscv_forwarding_pipeline.sv:303]
